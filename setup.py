192384b6127ac554e1fd3cc9dd9de0c90b3ac3ac
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2016-11-28 11:22:22 -0500

    setup.py: xivo-pbx -> wazo-pbx

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     author='Avencall',
     author_email='dev@avencall.com',
 
-    url='https://github.com/xivo-pbx/xivo-provd-cli',
+    url='https://github.com/wazo-pbx/xivo-provd-cli',
 
     packages=find_packages(),
     scripts=['bin/xivo-provd-cli'],
